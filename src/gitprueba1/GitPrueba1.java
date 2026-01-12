0c3baa8caf7b6fda738a846a314c569e0f7eab31
danielromero <danielromeroroba.marco@gmail.com>
2026-01-12 18:50:19 +0100

    He empezado de 0 mi proyecto

--- src/gitprueba1/GitPrueba1.java
+++ src/gitprueba1/GitPrueba1.java
@@ -14,7 +14,7 @@ public class GitPrueba1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Voy a empezar de 0");
     }
     
 }
