47d562d70641d7e423321d1713e1492fded7375a
danielromero <danielromeroroba.marco@gmail.com>
2026-01-16 17:41:42 +0100

    He terminado el login

--- src/gitprueba1/GitPrueba1.java
+++ src/gitprueba1/GitPrueba1.java
@@ -14,7 +14,7 @@ public class GitPrueba1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Voy a empezar de 0");
+        System.out.println("Primera Prueba");
         System.out.println("Segunda Prueba");
         System.out.println("Tercera Prueba");
     }
---
+++ src/gitprueba1/login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitprueba1;
+
+/**
+ *
+ * @author IFC303_1
+ */
+public class login {
+    void validarCorreo() {
+        System.out.println("Estoy empezando a crear el login...");
+        System.out.println("He terminado de crear el login...");
+    }
+}
